d10190cd787704c7c0c0251469a367b27cf26a35
Jalal <celal7771997@gmail.com>
2022-05-05 09:27:29 +0300

    if else условия

--- main.js
+++ main.js
@@ -1,20 +1,38 @@
-const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '')
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    private: false
-};
+// const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '')
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     private: false
+// };
+//
+// const a = prompt('Один из последних просмотренных фильмов?', '');
+// const b = prompt('На сколько оцените его?', '');
+// const c = prompt('Один из последних просмотренных фильмов?', '');
+// const d = prompt('На сколько оцените его?', '');
+//
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+//
+// console.log(personalMovieDB)
+//
+// console.log(personalMovieDB.movies['логан'])
 
-const a = prompt('Один из последних просмотренных фильмов?', '');
-const b = prompt('На сколько оцените его?', '');
-const c= prompt('Один из последних просмотренных фильмов?', '');
-const d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB)
 
-console.log(personalMovieDB.movies['логан'])
+// Условия
+
+
+
+if (4) {
+    console.log('Ok!');
+}else {
+    console.log('Error')
+}
+
+const num = 4;
+
+(num===4) ? console.log('OK!') : console.log('Error')
+
